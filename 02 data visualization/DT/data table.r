fa475029690d8bfea91b2c6fd80a4ea306c66f6f
jeremy001 <diyiduan@126.com>
2017-03-09 15:49:12 +0800

    add data.table.r

---
+++ 02 data visualization/DT/data table.r
@@ -0,0 +1,40 @@
+# loading packages
+library(DT)
+
+# basic usage
+datatable(head(iris))
+
+# edit class argument
+datatable(head(iris), class = 'cell-border stripe')
+
+# edit style argument
+# "default", "bootstrap", "bootstrap4", "foundation", "jqueryui", "material", "semanticui", "uikit"
+datatable(iris, style = 'default')
+
+# rownames 默认显示行名
+datatable(iris, rownames = FALSE)
+datatable(head(iris, 26), rownames = LETTERS)
+
+# colnames 可自定义
+datatable(iris, colnames = c(head(LETTERS, 5)))
+datatable(iris, colnames = c('中文', '中', '我', '哈哈', 'e')) # 可以用中文字符
+datatable(iris, colnames = c('ID' = 1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- good tips.r
+++ good tips.r
@@ -1,4 +1,4 @@
-# 1. load multiple packages once
+# 1. load multiple packages at once
 ipak <- function(pkg){
   new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
   if (length(new.pkg)) 
@@ -8,4 +8,10 @@ ipak <- function(pkg){
 packages <- c("ggplot2", "RJDBC", "recharts", "dplyr", "shiny", "shinydashboard", "DT", "leaflet", "sqldf", "tidyr")
 ipak(packages)
 
+# 2. update all packages at once
+install.packages( 
+  lib  = lib <- .libPaths()[1],
+  pkgs = as.data.frame(installed.packages(lib), stringsAsFactors=FALSE)$Package,
+  type = 'source'
+)
 
