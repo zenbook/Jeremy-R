e0a0abb8d556bff9ab1a247f2885cca16138c149
jeremy001 <diyiduan@126.com>
2017-04-03 09:42:51 +0800

    update some files

--- 01 data processing/quantmod.r
+++ 01 data processing/quantmod.r
@@ -4,6 +4,7 @@ library('quantmod')
 # get one company's data
 getSymbols('GOOG')
 class(GOOG)
+head(GOOG)
 ## get multiple companies data
 getSymbols('GOOG;AAPL;AMZN;MSFT')
 ## 获取指定时间段的数据
@@ -11,4 +12,14 @@ getSymbols('GOOG', from = '2017-01-01')
 getSymbols('GOOG', from = '2016-01-01', to = '2016-04-30')
 
 # 绘制股票图
-chartSeries(getSymbols("GOOG", from = '2017-01-01'))
+chartSeries(GOOG)
+
+# 为股票图添加元素
+addMACD()
+addBBands()
+
+# 绘制股票图，设置颜色和主题
+chartSeries(GOOG, multi.col = TRUE, theme = 'white')
+
+# 绘制股票图，添加其他元素
+chartSeries(GOOG, multi.col = TRUE, theme = 'white', TA = 'addMACD(); addBBands()')
