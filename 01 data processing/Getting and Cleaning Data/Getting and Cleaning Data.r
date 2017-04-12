9cc299ccfb41ce9d39ccf5f753d7fea39ee322f1
jeremy001 <diyiduan@126.com>
2017-04-12 22:23:16 +0800

    update getting and cleaning data.r file

--- 01 data processing/Getting and Cleaning Data/Getting and Cleaning Data.r
+++ 01 data processing/Getting and Cleaning Data/Getting and Cleaning Data.r
@@ -11,14 +11,14 @@ setwd("../")
 setwd("C:/Users/dakongyi")
 getwd()
 
-# download file from web
+# download file from web ----------------------------------
 ## download.file()
 ## window系统下，如果url使用的https协议，则可能无法下载数据 
 fileurl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
 download.file(fileurl, destfile = './cameras.csv', method = 'curl')
 list.files()
 
-# read local files
+# read local files ----------------------------------------
 ## read.table()
 cameras <- read.table(file = 'Baltimore_Fixed_Speed_Cameras.csv',
                       sep = ',',
@@ -29,7 +29,7 @@ cameras <- read.csv(file = 'Baltimore_Fixed_Speed_Cameras.csv',
                     header = TRUE)
 head(cameras)
 
-# read xlsx file 
+# read xlsx file ------------------------------------------
 ## load library
 library("xlsx")
 ## function structure
@@ -57,6 +57,46 @@ read.xlsx(file = "Baltimore_Fixed_Speed_Cameras.xlsx",
           sheetIndex = 1,
           colIndex = colindexs,
           rowIndex = rowindexs)
+
+# read xml file -------------------------------------------
+library('XML')
+# case 1
+fileurl <- "http://w3school.com.cn/example/xmle/simple.xml"
+doc <- xmlTreeParse(fileurl, useInternal = TRUE)
+rootNode <- xmlRoot(doc)
+xmlName(rootNode)
+names(rootNode)
+rootNode[[1]]
+rootNode[[1]][1]
+xmlSApply(rootNode, xmlValue)
+xpathSApply(rootNode, "//name", xmlValue)
+xpathSApply(rootNode, "//price", xmlValue)
+
+# case 2
+fileurl <- "http://espn.go.com/nfl/team/_/name/bal/baltimore-ravens"
+doc <- htmlTreeParse(fileurl, useInternal = TRUE)
+scores <- xpathSApply(doc, "//li[@class='scores']", xmlValue)
+teams <- xpathSApply(doc, "//li[@class='team-name']", xmlValue) # 标签可能换了
+scores
+teams
+
+# good doc about extract data from xml file
+# https://www.stat.berkeley.edu/~statcur/Workshop2/Presentations/XML.pdf
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
