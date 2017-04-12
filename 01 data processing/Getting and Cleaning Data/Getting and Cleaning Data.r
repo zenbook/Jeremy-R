20251549aa2aeda1d40156e46c8c96602ec91b5d
jeremy001 <diyiduan@126.com>
2017-04-12 21:41:54 +0800

    add rvest.r file

---
+++ 01 data processing/Getting and Cleaning Data/Getting and Cleaning Data.r
@@ -0,0 +1,63 @@
+# this note is based on the cource from courera
+# https://www.coursera.org/learn/data-cleaning/home/welcome
+library('rstudioapi')
+# 将当前文件所在的目录设置为工作目录
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# week 01  -------------------------------------------------------------------------
+# about working directory
+setwd("./data")
+setwd("../")
+setwd("C:/Users/dakongyi")
+getwd()
+
+# download file from web
+## download.file()
+## window系统下，如果url使用的https协议，则可能无法下载数据 
+fileurl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
+download.file(fileurl, destfile = './cameras.csv', method = 'curl')
+list.files()
+
+# read local files
+## read.table()
+cameras <- read.table(file = 'Baltimore_Fixed_Speed_Cameras.csv',
+                      sep = ',',
+                      header = TRUE)
+head(cameras)
+## read.csv()
+cameras <- read.csv(file = 'Baltimore_Fixed_Speed_Cameras.csv',
+                    header = TRUE)
+head(cameras)
+
+# read xlsx file 
+## load library
+library("xlsx")
+## function structure
+read.xlsx(file = ,  # xlsx文件名称(包括路径)
+          sheetIndex = ,  # 需读取的sheet页序号
+          sheetName = ,  # 需读取的sheet页名称
+          rowIndex = ,  # 需读取的行号
+          startRow = ,  # 开始读取的行号(当不设置rowIndex时，此项可设置)
+          endRow = ,  # 最后读取的行号(当不设置rowIndex时，此项可设置)
+          colIndex = ,  # 需读取的列号
+          as.data.frame = ,  # 是否保存为dataframe,默认为TRUE
+          header = ,  # 是否有表头,默认为TRUE,如果设置为FALSE,则第一行为第一个样本
+          colClasses = ,  # 一个向量，设置每列的数据类型
+          keepFormulas = ,  # 单元格中的公式是否保存为文本
+          encoding = ,  # 设置读取的文本的编码格式
+          stringsAsFactors = )  # 文本是否转成因子
+
+cameras <- read.xlsx(file = "Baltimore_Fixed_Speed_Cameras.xlsx",
+                     sheetIndex = 1,
+                     header = TRUE)
+head(cameras)
+colindexs <- c(2:3)
+rowindexs <- c(1:5)
+read.xlsx(file = "Baltimore_Fixed_Speed_Cameras.xlsx",
+          sheetIndex = 1,
+          colIndex = colindexs,
+          rowIndex = rowindexs)
+
+
+
+
