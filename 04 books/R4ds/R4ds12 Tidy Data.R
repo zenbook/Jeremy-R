8102ddf68a030925ca58e43579fc87eacbe9724a
jeremy001 <diyiduan@126.com>
2017-05-14 22:15:48 +0800

    r4ds

--- 04 books/R4ds/R4ds12 Tidy Data.R
+++ 04 books/R4ds/R4ds12 Tidy Data.R
@@ -24,14 +24,26 @@ ggplot(table1) +
            stat = 'identity', 
            position = 'dodge')
 
-# 12.3 spreading and gathering
+# 12.3 spreading and gathering ============================================
 
+## 12.3.1 gathering
+table4a %>% 
+  gather(`1999`, `2000`, key = 'year', value = 'cases')
 
+table4b %>% 
+  gather(`1999`, `2000`, key = 'year', value = 'population')
 
+tidy4a <- table4a %>% 
+  gather(-country, key = 'year', value = 'cases')
 
+tidy4b <- table4b %>% 
+  gather(-country, key = 'year', value = 'population')
 
+left_join(tidy4a, tidy4b)
 
-
+## 12.3.2 spreading
+## spread正是gather的逆操作
+spread(table2, key = type, value = count)
 
 
 
